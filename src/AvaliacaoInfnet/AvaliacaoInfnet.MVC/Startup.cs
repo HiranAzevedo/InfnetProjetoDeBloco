ed1e8aebb0feb0817e32f52e9ac4a3b8bac31287
Hiran <hiran.tassinari@gmail.com>
2017-03-30 19:47:51 -0300

    Criando o projeto e atualizacao do projeto

---
+++ src/AvaliacaoInfnet/AvaliacaoInfnet.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AvaliacaoInfnet.MVC.Startup))]
+namespace AvaliacaoInfnet.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
