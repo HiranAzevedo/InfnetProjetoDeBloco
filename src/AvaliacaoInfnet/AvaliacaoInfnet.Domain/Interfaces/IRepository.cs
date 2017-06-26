55fcc90e3b5561179e8d5f9d2f60d786fa50771e
Hiran <hiran.tassinari@gmail.com>
2017-06-26 17:49:15 -0300

    Adicionando repository

---
+++ src/AvaliacaoInfnet/AvaliacaoInfnet.Domain/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AvaliacaoInfnet.Domain.Interfaces
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+
+        TEntity GetById(int id);
+
+        TEntity Add(TEntity entity);
+
+        TEntity Update(TEntity entity);
+
+        void Remove(int id);
+
+    }
+}
---
+++ src/AvaliacaoInfnet/AvaliacaoInfnet.Persistencia/Repositorio/RepositoryBase.cs
@@ -0,0 +1,46 @@
+﻿using AvaliacaoInfnet.Persistencia.Contexto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AvaliacaoInfnet.Persistencia.Repositorio
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        protected AvaliacaoInfnetContext Db = new AvaliacaoInfnetContext();
+
+        public void Add(TEntity obj)
+        {
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+            //return Db.Set<TEntity>().AsNoTracking().ToList(); // Não usa LazyLoad se não há tracking.
+        }
+
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
