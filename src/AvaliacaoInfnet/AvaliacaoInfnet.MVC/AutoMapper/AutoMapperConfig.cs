cc9119b4be42c0059e5c459aed2c592853955a84
Victormblopes <victormblopes@gmail.com>
2017-06-27 22:37:07 -0300

    AutoMapper criado, mas as classes de mapeamento estão comentadas pois ainda não criamos as ViewModels. Quando criar é só descomentar!

---
+++ src/AvaliacaoInfnet/AvaliacaoInfnet.MVC/AutoMapper/AutoMapperConfig.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace AvaliacaoInfnet.MVC.AutoMapper
+{
+    public class AutoMapperConfig
+    {
+
+        public static void RegisterMappings()
+        {
+            Mapper.Initialize(x =>
+            {
+                x.AddProfile<DomainToViewModelMappingProfile>();
+                x.AddProfile<ViewModelToDomainMappingProfile>();
+            }
+            );
+        }
+
+    }
+}
--- src/AvaliacaoInfnet/AvaliacaoInfnet.MVC/Global.asax.cs
+++ src/AvaliacaoInfnet/AvaliacaoInfnet.MVC/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AvaliacaoInfnet.MVC.AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -18,6 +19,7 @@ namespace AvaliacaoInfnet.MVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutoMapperConfig.RegisterMappings();
         }
     }
 }
