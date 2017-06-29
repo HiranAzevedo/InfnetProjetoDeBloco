444640b15ed51255ee3974383d3dfaefe88d5df3
Marcus Moreno <moreno.mvv@gmail.com>
2017-06-29 19:15:05 -0300

    Add migrations 2

---
+++ src/AvaliacaoInfnet/AvaliacaoInfnet.Persistencia/Migrations/201706292207296_002.cs
@@ -0,0 +1,16 @@
+namespace AvaliacaoInfnet.Persistencia.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _002 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
