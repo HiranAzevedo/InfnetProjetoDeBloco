9ef47f1ac3265b413cf2c093456585ffcd0cbdeb
Marcus Moreno <moreno.mvv@gmail.com>
2018-02-18 14:43:20 -0300

    Começa adicionar os configurations de repositorio

---
+++ src/CourseRating/CourseRating.Data/Config/UsuarioConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using CourseRating.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseRating.Data.Config
+{
+   public  class UsuarioConfiguration : EntityTypeConfiguration<Usuario>
+    {
+        public UsuarioConfiguration()
+        {
+            HasKey(x => x.Cpf);
+        }
+    }
+}
