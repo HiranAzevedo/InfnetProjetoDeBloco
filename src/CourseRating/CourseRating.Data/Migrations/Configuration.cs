97b5ae67f112d270d0534fb67d0ecd2f5de349ca
Marcus Moreno <moreno.mvv@gmail.com>
2018-02-26 20:43:32 -0300

    Adiciona migrations

--- src/CourseRating/CourseRating.Data/Migrations/Configuration.cs
+++ src/CourseRating/CourseRating.Data/Migrations/Configuration.cs
@@ -1,15 +1,18 @@
 namespace CourseRating.Data.Migrations
 {
+    using System;
+    using System.Data.Entity;
     using System.Data.Entity.Migrations;
+    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Context.CourseRatingContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<CourseRating.Data.Context.CourseRatingContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(Context.CourseRatingContext context)
+        protected override void Seed(CourseRating.Data.Context.CourseRatingContext context)
         {
             //  This method will be called after migrating to the latest version.
 
